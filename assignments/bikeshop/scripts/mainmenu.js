0eefaea10febff87b1a76da12ee7f8bde7907e7d
Swanks <35353813+Swanks@users.noreply.github.com>
2018-04-08 23:15:51 -0700

    updates

--- assignments/bikeshop/scripts/mainmenu.js
+++ assignments/bikeshop/scripts/mainmenu.js
@@ -1,3 +1,10 @@
+// Hamburger menu script
+
+function toggleNavMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+// Wayfinding element
 //Active navigation script
 
 //remember to add an ID of "mainmenu" to the nav ul
@@ -17,3 +24,46 @@ if (pageHREF !=="") {
         }  // end if
     }  // end of the search for a match
 }  //end of the if !==
+
+
+// Current date script
+
+var date = new Date();
+
+var weekDay = new Array(7);
+weekDay[0] = "Sunday";
+weekDay[1] = "Monday";
+weekDay[2] = "Tuesday";
+weekDay[3] = "Wednesday";
+weekDay[4] = "Thursday";
+weekDay[5] = "Friday";
+weekDay[6] = "Saturday";
+
+var month = new Array(12);
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+var weekday = weekDay[date.getDay()];
+var whichmonth = month[date.getMonth()];
+
+document.getElementById("currentdate").innerHTML = weekday + ", " + date.getDate() + " " + whichmonth + " " + date.getFullYear();
+
+
+// Severity slider script
+
+function sliderInput(custRating) {
+    document.getElementById("severityRating").innerHTML = custRating;
+}
+
+
+
